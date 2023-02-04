a5556d3d9a83b4bee2296fea5ff01b37006a5943
Akram <khalilovakram07@gmail.com>
2023-02-04 18:03:05 +0600

    add json

--- src/pages/ShopPage/ShopPage.jsx
+++ src/pages/ShopPage/ShopPage.jsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
 import img from '../../images/img1.png'
 import './shoppage.css'
 
 const ShopPage = () => {
+
+    const URL = "http://localhost:3004/posts"
+
+    const [data, setData] = useState([])
+
+    useEffect(() => {
+        axios.get(URL)
+            .then(res => setData(res.data))
+    }, [])
+
+    console.log(data);
+
     return (
         <div className='shop__wrapper'>
             <p>Показано: 9 из 12 товаров</p>
             <div className="cards">
-                <div className="card">
-                    <img src={img} alt="" />
+                {/* <img src={img} alt="" />
                     <p>Футболка USA</p>
-                    <p>$229</p>
-                </div>
+                    <p>$229</p> */}
+                {data.map(item => {
+                    return (
+                        <div className="card">
+                            <p>{item.title}</p>
+                            <p>{item.price}</p>
+                        </div>
+                    )
+                })}
 
-                <div className="card">
-                <img src={img} alt="" />
+                {/* ctrl + c = y */}
+
+                {/* <div className="card">
+                    <img src={img} alt="" />
                     <p>Футболка USA</p>
                     <p>$229</p>
                 </div>
@@ -22,7 +43,7 @@ const ShopPage = () => {
                     <img src={img} alt="" />
                     <p>Футболка USA</p>
                     <p>$229</p>
-                </div>
+                </div> */}
             </div>
         </div>
     );
